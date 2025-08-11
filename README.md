a01011fe593e2d9052c0395e468959d7776e5020
BarackOBusiness <nichjoh21@gmail.com>
2025-08-11 01:05:03 -0400

    Initial set of patches for competition in cavern
    between me and squilly that's right none of you are ever going to grind
    this map even with this plugin.

---
+++ README.md
@@ -0,0 +1,7 @@
+# GOI Competitive Patches
+This plugin exists to organize patches to certain scenes or categories intended to make competition more consistent or fix bugs.\
+It is not the goal of this plugin to do so without making any changes to game physics so well established categories like glitchless are off limits
+unless the will of the playerbase to make sweeping changes to said competitive scenes does arise.
+
+## Current Patches
+- Make all rigidbodies in cavern map use continuous collision detection mode to reduce clipping into terrain and improve consistency of hammer traction
---
+++ src/Main.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CompetitivePatches
+{
+    [BepInPlugin("goi.ext.competitivepatches", "Competitive Patches", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Competitive patches have loaded!");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode _mode)
+        {
+            switch (scene.name) {
+                case "Gems and Minerals": // Cavern map
+                    GameObject rotary = GameObject.Find("Rotary");
+                    Rigidbody2D[] bodies = rotary.GetComponentsInChildren<Rigidbody2D>();
+                    foreach (Rigidbody2D body in bodies) {
+                        body.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+                    }
+                    break;
+                default: // Do nothing to other scenes
+                    break;
+            }
+        }
+    }
+}
